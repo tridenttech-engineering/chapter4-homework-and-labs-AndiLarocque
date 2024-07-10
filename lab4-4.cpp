e0945d186b1dd4890f02cf33a53b51099b8affb5
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-10 19:55:59 -0400

    Update lab4-4.cpp

--- lab4-4.cpp
+++ lab4-4.cpp
@@ -1,23 +1,33 @@
 //Lab4-4.cpp - displays the volume of a cylinder
 //Created/revised by <your name> on <current date>
+
 #include <iostream>
 using namespace std;
 int main()
-{
-  //declare named constant and variables 
-  double height   = 0.0;
-  double radius   = 0.0;
-  double volume   = 0.0;
-  double PI = 3.14159; 
+{ 
+  //declare named constant and variables
+  
+  double height   = 0.0; // moved outside of the main function
+  double radius   = 0.0; 
+  double volume   = 0.0; 
+  double PI = 3.14; 
   
   //enter input items
 cout << "Height: ";
-cout << "Radius: ";
 cin >> height;
+cout << "Radius: ";
+cin >> radius;
 
   //calculate and display the volume
 volume = PI * radius * radius * height;
-cout << "Volume: " << volume << endl;
+cout << volume << endl;
   
 return 0;
+
 } //end of main function
+ 
+
+
+  
+  
+  
