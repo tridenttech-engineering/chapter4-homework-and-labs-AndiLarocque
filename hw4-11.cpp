3201f06b345e7fa9689c422207868817867d6900
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-10 20:51:28 -0400

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -5,7 +5,26 @@
 using namespace std;
 
 int main() {
+ 
+  //declare name and constant variables
+  double beginningbalance = 0.0;
+  double totaldeposits = 0.0;
+  double totalwithdraws = 0.0;
+  double accountbalance = 0.0; 
+  double totalbalance = 0.0;
   
-
+  //enter input items
+  cout << "Enter the beginning balance: "; 
+  cin >> beginningbalance; 
+  cout << "Enter the total deposits: "; 
+  cin >> totaldeposits;
+cout << "Enter the total withdraws: "; 
+  cin >> totalwithdraws; 
+  
+  //calculate and display 
+  totalbalance = beginningbalance + totaldeposits; 
+  accountbalance = totalbalance - totalwithdraws; 
+  cout << accountbalance << endl; 
+  
   return 0;
 } // end of main function
